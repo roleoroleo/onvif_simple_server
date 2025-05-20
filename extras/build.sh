3e9f4b69559953e73c3dde468fa8f3801198ef3b
roleo <roleo@iperbole.bologna.it>
2025-05-20 21:40:00 +0200

    Clean unused parts in build.sh

--- extras/build.sh
+++ extras/build.sh
@@ -4,14 +4,6 @@
 # This script will compile lighttpd, tls library and the server
 # and will prepare a folder with all the files you need.
 
-# If you want to cross-compile to a different platform,
-# source your environment here: $CC, $CXX, $STIP, etc...
-# and set CROSS_COMPILE_HOST variable
-
-#. /home/user/cross-compile/env
-#CROSS_COMPILE_HOST="--host=arm-openwrt-linux"
-CROSS_COMPILE_HOST=""
-
 INSTALL_DIR="_install"
 export HAVE_MBEDTLS=1
 LIGHTTPD=lighttpd-1.4.73
@@ -36,7 +28,7 @@ tar zxvf $LIGHTTPD.tar.gz
 cd $LIGHTTPD
 
 # Run configure with minimal options
-./configure $CROSS_COMPILE_HOST --disable-ipv6 --without-pcre2 --without-zlib
+./configure --disable-ipv6 --without-pcre2 --without-zlib
 
 make
 cd ..
@@ -60,35 +52,6 @@ echo "" >> $INSTALL_DIR/etc/lighttpd.conf
 echo "server.modules = ( \"mod_cgi\" )" >> $INSTALL_DIR/etc/lighttpd.conf
 echo "cgi.assign = ( \"_service\" => \"\" )" >> $INSTALL_DIR/etc/lighttpd.conf
 
-### TLS ###
-# Build mbedtls or libtomcrypt
-if [ "$HAVE_MBEDTLS" == "1" ]; then
-    if [ ! -f v3.4.0.tar.gz ]; then
-        wget https://github.com/Mbed-TLS/mbedtls/archive/refs/tags/v3.4.0.tar.gz
-    fi
-    tar zxvf ./v3.4.0.tar.gz
-    if [ ! -L mbedtls ]; then
-        ln -s mbedtls-3.4.0 mbedtls
-    fi
-    cp -f mbedtls_config.h mbedtls/include/mbedtls/
-    cd mbedtls
-#    make clean
-    make no_test
-    cd ..
-else
-    if [ ! -f crypt-1.18.2.tar.xz ]; then
-        wget https://github.com/libtom/libtomcrypt/releases/download/v1.18.2/crypt-1.18.2.tar.xz
-    fi
-    tar Jxvf ./crypt-1.18.2.tar.xz
-    if [ ! -L libtomcrypt ]; then
-        ln -s libtomcrypt-1.18.2 libtomcrypt
-    fi
-    cd libtomcrypt
-#    make clean
-    CFLAGS="-DLTC_NOTHING -DLTC_SHA1 -DLTC_BASE64" make
-    cd ..
-fi
-
 ### ONVIF_SIMPLE_SERVER ###
 cd ..
 make
@@ -98,12 +61,16 @@ cp ../onvif_simple_server $INSTALL_DIR/www/onvif
 ln -s ./onvif_simple_server $INSTALL_DIR/www/onvif/device_service
 ln -s ./onvif_simple_server $INSTALL_DIR/www/onvif/events_service
 ln -s ./onvif_simple_server $INSTALL_DIR/www/onvif/media_service
+ln -s ./onvif_simple_server $INSTALL_DIR/www/onvif/media2_service
 ln -s ./onvif_simple_server $INSTALL_DIR/www/onvif/ptz_service
+ln -s ./onvif_simple_server $INSTALL_DIR/www/onvif/deviceio_service
 cp -R ../device_service_files $INSTALL_DIR/www/onvif
 cp -R ../events_service_files $INSTALL_DIR/www/onvif
 cp -R ../generic_files $INSTALL_DIR/www/onvif
 cp -R ../media_service_files $INSTALL_DIR/www/onvif
+cp -R ../media2_service_files $INSTALL_DIR/www/onvif
 cp -R ../ptz_service_files $INSTALL_DIR/www/onvif
+cp -R ../deviceio_service_files $INSTALL_DIR/www/onvif
 
 cp ../onvif_notify_server $INSTALL_DIR/bin || exit 1
 cp -R ../notify_files/* $INSTALL_DIR/etc/onvif_notify_server
